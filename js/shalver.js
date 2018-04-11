a5924d31fc2170845bcafda6cca3518c1d2d3467
payam <payam@ubuntu.ubuntu-domain>
2018-04-10 21:38:26 -0700

    added the js and added some basic js functionality to the code

---
+++ js/shalver.js
@@ -0,0 +1,36 @@
+
+
+// Aux functions
+// Copyright Shalver,
+// written April 10, 2018
+
+function setrange2textbox (slider_number) {
+
+    var sliderval = document.getElementsByClassName("range");
+    var outputval = document.getElementsByClassName("text");
+    outputval[slider_number].value = sliderval[slider_number].value;
+}
+
+function settextbox2range (textbox_number) {
+
+    var timeout   = null;
+    var sliderval = document.getElementsByClassName("range");
+    var inputval  = document.getElementsByClassName("text");
+    timeout       = setTimeout(function() {checkuserinput(inputval, textbox_number);}, 2000);
+}
+
+function checkuserinput (inputval, textbox_number) {
+
+
+    var sliderval = document.getElementsByClassName("range");
+    if(inputval[textbox_number].value > sliderval[textbox_number].max) {
+	inputval[textbox_number].value = sliderval[textbox_number].max;
+    }
+    else if(inputval[textbox_number].value < sliderval[textbox_number].min) {
+	inputval[textbox_number].value = sliderval[textbox_number].min;
+    }else {
+    sliderval[textbox_number].value = inputval[textbox_number].value;
+    }
+    
+}
+
